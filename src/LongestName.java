1d162dbae2bce15c0f286aea68d1c8acde2c8dd3
Antonio La Barba <alabarba@gmail.com>
2019-05-10 18:53:02 +0200

    First working version. Asks to enter some names and returns the longest

--- src/LongestName.java
+++ src/LongestName.java
@@ -3,26 +3,36 @@ import java.util.*;
 import java.lang.*;
 
 public class LongestName{
-          public static void main (String[] args){longestName();}
-
-
-          public static void longestName(Scanner console, int n){
+    
+	public static void main (String[] args){
+	String inputNames="";
+	System.out.print("Please insert the number of names ");
+    Scanner input = new Scanner(System.in);
+    int numberOfNames=input.nextInt();
+    for(int i =1; i<=numberOfNames; i++) {
+    	System.out.print("name #" + i +"? ");
+    	Scanner console= new Scanner(System.in);
+    	inputNames=inputNames + console.next() +"," ;
+    	//console.close(); To be understood why closing the Scanner object breaks the code.
+    	}
+    Scanner s = new Scanner(inputNames).useDelimiter(",");
+    longestN(s,numberOfNames);
+	}
+    
+          public static void longestN(Scanner console, int n){
           String longestName=" ";
           boolean tie=false;
 
-          String[] names=console.next().split("\n",0);
-          System.out.println(names);
-
           for (int i=1; i<=n; i++) {
-              System.out.print("name #" + i +"? ");
-              if (names[i].length() > longestName.length()) 
-              {longestName=(names[i].substring(0,1).toUpperCase()) + (names[i].substring(1).toLowerCase());}
-              if ((names[i].length() == longestName.length())) {tie=true;}
+        	  String a=console.next();
+        	  if (a.length() == longestName.length()) {tie=true;}
+        	  if (a.length() > longestName.length()) 
+              {longestName=a;}       
           }
 
           System.out.println(longestName + "\'s name is longest");
-          if (tie==true) {System.out.println("(There was a tie!");}
+          if (tie==true) {System.out.println("(There was a tie!)");}
           }
-  
+ 
   }
 
